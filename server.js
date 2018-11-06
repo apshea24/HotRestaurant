cff766dd167fb29634ee2cf6040cd2839a58681c
shaggsworth <austin.shea89@gmail.com>
2018-11-06 13:49:58 -0700

    initial_upload

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+
+const path = require("path");
+
+var PORT = process.env.PORT || 8000;
+let app = express();
+
+let reservations = [];
+let waitlist = [];
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT)
+});
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"))
+});
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"))
+});
+
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"))
+});
+
+app.get("/api/reservations", function(req, res) {
+    return res.json(reservations)
+});
+
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist)
+});
+
+app.post("/tables", function(req, res) {
+    let newRes = req.body;
+
+    if (reservations.length > 5) {
+        waitlist.push(newRes)
+    } else {
+        reservations.push(newRes)
+    }
+
+    console.log(req.body)
+
+
+    res.json(newRes);
+});
